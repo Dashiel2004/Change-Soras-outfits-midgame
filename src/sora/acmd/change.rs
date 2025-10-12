1e3ed91f80de239456caa246ddb97bbf5e1afa0f
Dashiel Padget <106357778+Dashiel2004@users.noreply.github.com>
2025-10-11 23:28:50 -0500

    Update change.rs
    added basic include functions to allow for use of Smashline, Skyline, and Smash_Script usage

--- src/sora/acmd/change.rs
+++ src/sora/acmd/change.rs
@@ -1,1 +1,12 @@
+use {
+    skyline::libc::c_uint, smash::{
+        app::{lua_bind::*, sv_animcmd::*, *},
+        lib::lua_const::*,
+        lua2cpp::*,
+        phx::*,
+    }, smash_script::*, smashline::*, std::hash
+
+};
+
+use std::sync::atomic::{AtomicBool, Ordering};
 
