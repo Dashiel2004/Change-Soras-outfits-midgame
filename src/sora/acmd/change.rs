adf3baf017c30c6aa03ba2579c0bbab82d7fb559
Dashiel Padget <106357778+Dashiel2004@users.noreply.github.com>
2025-10-16 16:12:30 -0500

    Add outfit visibility management with AtomicBool

--- src/sora/acmd/change.rs
+++ src/sora/acmd/change.rs
@@ -3,10 +3,25 @@ use {
         app::{lua_bind::*, sv_animcmd::*, *},
         lib::lua_const::*,
         lua2cpp::*,
-        phx::*,
+        phx::
     }, smash_script::*, smashline::*, std::hash
 
 };
 
 use std::sync::atomic::{AtomicBool, Ordering};
 
+
+
+let outfitDefault = AtomicBool::new(false);
+let outfit1 = AtomicBool::new(false);
+let outfit2 = AtomicBool::new(false);
+let outfit3 = AtomicBool::new(false);
+let outfit4 = AtomicBool::new(false);
+
+public unsafe fn oufitDefaultVisible(value: bool){
+outfitDefault.store(true, atomic::Ordering::Relaxed);
+outfit1.store(false, atomic::Ordering::Relaxed);
+outfit2.store(false, atomic::Ordering::Relaxed);
+outfit3.store(false, atomic::Ordering::Relaxed);
+outfit4.store(false, atomic::Ordering::Relaxed);
+}
