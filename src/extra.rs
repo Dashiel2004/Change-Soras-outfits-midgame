0803bbfd192b4e50d820668bc8312d43b3156e12
Dashiel Padget <106357778+Dashiel2004@users.noreply.github.com>
2025-11-11 17:16:57 -0600

    Add outfit management functions for Sora
    This file contains functions to manage Sora's outfits during various game events, including loading outfits on start, entry, and respawn, as well as handling win and lose conditions.

---
+++ src/extra.rs
@@ -0,0 +1,69 @@
+use {
+    smash::{
+        lua2cpp::*,
+        app::{lua_bind::*},
+        lib::{lua_const::*},
+    },
+    smashline::{*, Priority::*}
+};
+
+use crate::vars::*;
+use std::sync::atomic::{AtomicBool, Ordering};
+
+/// Loads the default outfit when Sora's model is reset
+pub unsafe extern "C" fn on_start(agent: &mut L2CAgentBase) {
+    load_default(agent);
+}
+/// Loads the default outfit when Sora's entrance animation plays
+pub unsafe extern "C" fn on_entry(agent: &mut L2CAgentBase) {
+    load_outfit1(agent);
+}
+/// Loads the default outfit when Sora respawns after being KO'd
+pub unsafe extern "C" fn trail_opff(agent: &mut L2CAgentBase) {
+    if StatusModule::status_kind(agent.module_accessor) == FIGHTER_STATUS_KIND_DEAD {
+        load_outfit1(agent);
+    }
+}
+
+pub unsafe extern "C" fn on_end(agent: &mut L2CAgentBase) {
+    if OUTFIT_WIN1.load(Ordering::Relaxed) {
+        load_outfit1(agent);
+    }
+    else if OUTFIT_WIN2.load(Ordering::Relaxed) {
+        load_outfit2(agent);
+    }
+    else if OUTFIT_WIN3.load(Ordering::Relaxed) {
+        load_outfit3(agent);
+    }
+    else if OUTFIT_WIN4.load(Ordering::Relaxed) {
+        load_outfit4(agent);
+    }
+    else if OUTFIT_WIN5.load(Ordering::Relaxed) {
+        load_outfit5(agent);
+    }
+    OUTFIT_WIN1.store(true, Ordering::Relaxed);
+    OUTFIT_WIN2.store(false, Ordering::Relaxed);
+    OUTFIT_WIN3.store(false, Ordering::Relaxed);
+    OUTFIT_WIN4.store(false, Ordering::Relaxed);
+    OUTFIT_WIN5.store(false, Ordering::Relaxed);
+
+}
+
+pub unsafe extern "C" fn on_lose(agent: &mut L2CAgentBase) {
+    hide_weapon(agent);
+}
+
+/// Installs the above functions
+pub fn install() {
+    Agent::new("trail")
+        .game_acmd("game_win1", on_end, Default)
+        .game_acmd("game_win2", on_end, Default)
+        .game_acmd("game_win3", on_end, Default)
+        .game_acmd("game_lose", on_lose, Default)
+        .game_acmd("game_entryl", on_entry, Default)
+        .game_acmd("game_entryr", on_entry, Default)
+        .on_start(on_start)
+        .on_line(Main, trail_opff)
+
+        .install();
+}
