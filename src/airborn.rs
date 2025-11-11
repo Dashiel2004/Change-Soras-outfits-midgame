981ef6cbf1f9ccb3e0090cc3db1130a5802d28a8
Dashiel Padget <106357778+Dashiel2004@users.noreply.github.com>
2025-11-11 17:18:28 -0600

    Implement airborne outfit change for Agent
    Adds functionality for Agent outfit changes based on dpad inputs during airborne situations.

---
+++ src/airborn.rs
@@ -0,0 +1,39 @@
+use {
+    smash::{
+        lua2cpp::*,
+        phx::*,
+        app::{sv_animcmd::*, lua_bind::*, *},
+        lib::{lua_const::*, L2CAgent, L2CValue},
+        hash40
+    },
+    smash_script::*,
+    smashline::{*, Priority::*}
+};
+use crate::vars::*;
+
+/// Made for online play, changes Sora's outfit based on dpad inputs
+pub unsafe extern "C" fn airborne_change(agent: &mut L2CAgentBase) {
+    let module_accessor = sv_system::battle_object_module_accessor(agent.lua_state_agent);
+    let situation_kind = StatusModule::situation_kind(module_accessor);    
+    if situation_kind == *SITUATION_KIND_AIR {
+        if ControlModule::check_button_on(agent.module_accessor, *CONTROL_PAD_BUTTON_APPEAL_HI){
+            load_outfit2(agent);
+        }
+        else if ControlModule::check_button_on(agent.module_accessor, *CONTROL_PAD_BUTTON_APPEAL_S_R){
+            load_outfit3(agent);
+        }
+        else if ControlModule::check_button_on(agent.module_accessor, *CONTROL_PAD_BUTTON_APPEAL_LW){
+            load_outfit4(agent);
+        }
+        else if ControlModule::check_button_on(agent.module_accessor, *CONTROL_PAD_BUTTON_APPEAL_S_L){
+            load_outfit5(agent);
+        }
+    }
+}
+
+/// Install function for online outfit change
+pub fn install() {
+    Agent::new("trail")
+        .on_line(Main, airborne_change)
+        .install();
+}
