da21bac72c59f3323f5b308549acd92458137305
Dashiel Padget <106357778+Dashiel2004@users.noreply.github.com>
2025-11-11 17:19:07 -0600

    Add new modules and update main function

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,15 @@
 #![feature(concat_idents, proc_macro_hygiene)]
 #![allow(unused_macros)]
 mod sora;
-
+mod config;
+mod vars;
+mod extra;
+mod effect;
+mod airborne;
+mod sound;
 #[skyline::main(name = "changing_outfit")]
 pub fn main() {
     sora::install();
+    extra::install();
+    airborne::install();
 }
