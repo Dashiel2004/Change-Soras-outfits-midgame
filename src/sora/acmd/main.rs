2f0607593e77deab76a500f1d7cd960f64e1bc79
Dashiel Padget <106357778+Dashiel2004@users.noreply.github.com>
2025-11-11 17:12:31 -0600

    Refactor outfit change handling and effects
    Refactor outfit change logic and remove unused variables. Implement outfit change effects and sounds based on configuration settings.

---
+++ src/sora/acmd/main.rs
@@ -0,0 +1,256 @@
+use {
+    smash::{
+        lua2cpp::*,
+        app::{sv_animcmd::*, lua_bind::*, *},
+        lib::{lua_const::*},
+    },
+    smash_script::*,
+    smashline::{*, Priority::*}
+};
+
+use crate::{config::Config};
+use std::sync::atomic::Ordering;
+
+use crate::vars::*;
+use crate::sound::*;
+use crate::effect::*;
+use crate::sora::acmd::default::*;
+
+pub unsafe extern "C" fn effect_utaunt(agent: &mut L2CAgentBase) {
+    let config = Config::get();
+    let color_slot = WorkModule::get_int(agent.module_accessor, *FIGHTER_INSTANCE_WORK_ID_INT_COLOR);
+    let enabled = if config.enable_change_outfit.all_slots {
+        true
+    } else{
+    match color_slot {
+        0 => config.enable_change_outfit.c00,
+        1 => config.enable_change_outfit.c01,
+        2 => config.enable_change_outfit.c02,
+        3 => config.enable_change_outfit.c03,
+        4 => config.enable_change_outfit.c04,
+        5 => config.enable_change_outfit.c05,
+        6 => config.enable_change_outfit.c06,
+        7 => config.enable_change_outfit.c07,
+        _ => false,
+    }
+    };
+    if enabled {
+        if !OUTFIT2.load(Ordering::Relaxed){
+            enter_outfit_change(agent);
+            load_outfit2(agent);
+            }
+        else{
+            exit_outfit_change(agent);
+            load_outfit1(agent);
+        }
+
+        frame(agent.lua_state_agent, 21.0);
+        if macros::is_excute(agent) {
+            ArticleModule::remove_exist(agent.module_accessor,*FIGHTER_TRAIL_GENERATE_ARTICLE_FLOWER,ArticleOperationTarget(*ARTICLE_OPE_TARGET_ALL));
+
+        }
+
+    }
+    else{
+        default_effect_utaunt(agent);
+    }
+}
+pub unsafe extern "C" fn sound_utaunt(agent: &mut L2CAgentBase) {
+    let config = Config::get();
+    let color_slot = WorkModule::get_int(agent.module_accessor, *FIGHTER_INSTANCE_WORK_ID_INT_COLOR);
+    let enabled = if config.enable_change_outfit.all_slots {
+        true
+    } else{
+    match color_slot {
+        0 => config.enable_change_outfit.c00,
+        1 => config.enable_change_outfit.c01,
+        2 => config.enable_change_outfit.c02,
+        3 => config.enable_change_outfit.c03,
+        4 => config.enable_change_outfit.c04,
+        5 => config.enable_change_outfit.c05,
+        6 => config.enable_change_outfit.c06,
+        7 => config.enable_change_outfit.c07,
+        _ => false,
+    }
+    };
+    if enabled{
+        if !OUTFIT2.load(Ordering::Relaxed){
+            sound_enter_outfit_change(agent);
+            }
+        else{
+            sound_exit_outfit_change(agent);
+        }
+
+    }
+    else{
+        default_sound_utaunt(agent);
+    }
+}
+
+pub unsafe extern  "C" fn effect_staunt(agent: &mut L2CAgentBase) {
+    let config = Config::get();
+    let color_slot = WorkModule::get_int(agent.module_accessor, *FIGHTER_INSTANCE_WORK_ID_INT_COLOR);
+    let enabled = if config.enable_change_outfit.all_slots {
+        true
+    } else{
+    match color_slot {
+        0 => config.enable_change_outfit.c00,
+        1 => config.enable_change_outfit.c01,
+        2 => config.enable_change_outfit.c02,
+        3 => config.enable_change_outfit.c03,
+        4 => config.enable_change_outfit.c04,
+        5 => config.enable_change_outfit.c05,
+        6 => config.enable_change_outfit.c06,
+        7 => config.enable_change_outfit.c07,
+        _ => false,
+    }
+    };
+    if enabled{
+        if ControlModule::check_button_on(agent.module_accessor, *CONTROL_PAD_BUTTON_APPEAL_S_R){
+            if !OUTFIT3.load(Ordering::Relaxed){
+                enter_outfit_change(agent);
+                load_outfit3(agent);
+
+            }
+            
+            else{
+                exit_outfit_change(agent);
+                load_outfit1(agent);
+            }
+    }
+        else if ControlModule::check_button_on(agent.module_accessor, *CONTROL_PAD_BUTTON_APPEAL_S_L){
+            if !OUTFIT5.load(Ordering::Relaxed){
+                enter_outfit_change(agent);
+                load_outfit5(agent);
+            }
+            
+            else{
+                exit_outfit_change(agent);
+                load_outfit1(agent);
+
+            }
+        }
+    }
+    else{
+        default_effect_staunt(agent);
+        }
+}
+
+pub unsafe extern  "C" fn sound_staunt(agent: &mut L2CAgentBase) {
+    let config = Config::get();
+    let color_slot = WorkModule::get_int(agent.module_accessor, *FIGHTER_INSTANCE_WORK_ID_INT_COLOR);
+    let enabled = if config.enable_change_outfit.all_slots {
+        true
+    } else{
+    match color_slot {
+        0 => config.enable_change_outfit.c00,
+        1 => config.enable_change_outfit.c01,
+        2 => config.enable_change_outfit.c02,
+        3 => config.enable_change_outfit.c03,
+        4 => config.enable_change_outfit.c04,
+        5 => config.enable_change_outfit.c05,
+        6 => config.enable_change_outfit.c06,
+        7 => config.enable_change_outfit.c07,
+        _ => false,
+    }
+    };
+    if enabled{
+        if ControlModule::check_button_on(agent.module_accessor, *CONTROL_PAD_BUTTON_APPEAL_S_R){
+            if !OUTFIT3.load(Ordering::Relaxed){
+                sound_enter_outfit_change(agent);
+            }
+            else{
+                sound_exit_outfit_change(agent);
+            }
+        }
+        else if ControlModule::check_button_on(agent.module_accessor, *CONTROL_PAD_BUTTON_APPEAL_S_L){
+            if !OUTFIT5.load(Ordering::Relaxed){
+                sound_enter_outfit_change(agent);
+            }
+            else{
+                sound_exit_outfit_change(agent);
+            }
+        }
+    }
+    else{
+        default_sound_staunt(agent);
+        }
+}
+
+pub unsafe extern  "C" fn effect_dtaunt(agent: &mut L2CAgentBase) {
+    let config = Config::get();
+    let color_slot = WorkModule::get_int(agent.module_accessor, *FIGHTER_INSTANCE_WORK_ID_INT_COLOR);
+    let enabled = if config.enable_change_outfit.all_slots {
+        true
+    } else{
+    match color_slot {
+        0 => config.enable_change_outfit.c00,
+        1 => config.enable_change_outfit.c01,
+        2 => config.enable_change_outfit.c02,
+        3 => config.enable_change_outfit.c03,
+        4 => config.enable_change_outfit.c04,
+        5 => config.enable_change_outfit.c05,
+        6 => config.enable_change_outfit.c06,
+        7 => config.enable_change_outfit.c07,
+        _ => false,
+    }
+    };
+    if enabled{
+        if !OUTFIT4.load(Ordering::Relaxed) {
+            enter_outfit_change(agent);
+            load_outfit4(agent);
+        } else {
+            exit_outfit_change(agent);
+            load_outfit1(agent);
+        }
+    } else {
+        default_effect_dtaunt(agent);
+    }
+}
+
+pub unsafe extern  "C" fn sound_dtaunt(agent: &mut L2CAgentBase) {
+    let config = Config::get();
+    let color_slot = WorkModule::get_int(agent.module_accessor, *FIGHTER_INSTANCE_WORK_ID_INT_COLOR);
+    let enabled = if config.enable_change_outfit.all_slots {
+        true
+    } else{
+    match color_slot {
+        0 => config.enable_change_outfit.c00,
+        1 => config.enable_change_outfit.c01,
+        2 => config.enable_change_outfit.c02,
+        3 => config.enable_change_outfit.c03,
+        4 => config.enable_change_outfit.c04,
+        5 => config.enable_change_outfit.c05,
+        6 => config.enable_change_outfit.c06,
+        7 => config.enable_change_outfit.c07,
+        _ => false,
+    }
+    };
+    if enabled{
+        if !OUTFIT4.load(Ordering::Relaxed) {
+            sound_enter_outfit_change(agent);
+        } else {
+            sound_exit_outfit_change(agent);
+        }
+    } else {
+        default_sound_dtaunt(agent);
+    }
+}
+
+
+pub fn install() {
+    Agent::new("trail")
+        .effect_acmd("effect_appealhir", effect_utaunt, Default)
+        .effect_acmd("effect_appealhil", effect_utaunt, Default)
+        .effect_acmd("effect_appeallwl", effect_dtaunt, Default)
+        .effect_acmd("effect_appeallwr", effect_dtaunt, Default)
+        .effect_acmd("effect_appealsr", effect_staunt, Default)
+        .effect_acmd("effect_appealsl", effect_staunt, Default)
+        .sound_acmd("sound_appealhir", sound_utaunt, Default)
+        .sound_acmd("sound_appealhil", sound_utaunt, Default)
+        .sound_acmd("sound_appeallwl", sound_dtaunt, Default)
+        .sound_acmd("sound_appeallwr", sound_dtaunt, Default)
+        .sound_acmd("sound_appealsr", sound_staunt, Default)
+        .sound_acmd("sound_appealsl", sound_staunt, Default)
+        .install();
+}
